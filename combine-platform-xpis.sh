023773d09a73b3484b7289cb5a1683cb1c407eea
L. David Baron <dbaron@dbaron.org>
2008-02-15 17:53:08 -0800

    Quote parameters correctly (and fix a missing dollar sign from previous change).

--- combine-platform-xpis.sh
+++ combine-platform-xpis.sh
@@ -43,10 +43,10 @@ then
 	exit 1
 fi
 
-DEST=$1
+DEST="$1"
 shift
 
-if [ -e $DEST ]
+if [ -e "$DEST" ]
 then
 	echo "Destination file $DEST already exists." 1>&2
 	exit 2
@@ -54,51 +54,51 @@ fi
 
 OLDUMASK=$(umask)
 umask 077
-TMPDIR=$(mktemp -d) || exit 2
+TMPDIR="$(mktemp -d)" || exit 2
 
 for SRC in "$@"
 do
-	mkdir $TMPDIR/in
-	unzip -d $TMPDIR/in $SRC > /dev/null
-	PLATFORM=$(grep targetPlatform $TMPDIR/in/install.rdf | cut -d\> -f2 | cut -d\< -f1)
-	echo Merging $PLATFORM 1>&2
-	mkdir $TMPDIR/in/platform
-	mkdir $TMPDIR/in/platform/$PLATFORM
-	mkdir $TMPDIR/in/platform/$PLATFORM/components
-	(cd $TMPDIR/in && find components -type f -a -not -name "*.xpt") | while read BINARY
+	mkdir "$TMPDIR/in"
+	unzip -d "$TMPDIR/in" "$SRC" > /dev/null
+	PLATFORM=$(grep targetPlatform "$TMPDIR/in/install.rdf" | cut -d\> -f2 | cut -d\< -f1)
+	echo "Merging $PLATFORM" 1>&2
+	mkdir "$TMPDIR/in/platform"
+	mkdir "$TMPDIR/in/platform/$PLATFORM"
+	mkdir "$TMPDIR/in/platform/$PLATFORM/components"
+	(cd "$TMPDIR/in" && find components -type f -a -not -name "*.xpt") | while read BINARY
 	do
-		mv $TMPDIR/in/$BINARY $TMPDIR/in/platform/$PLATFORM/$BINARY
+		mv "$TMPDIR/in/$BINARY" "$TMPDIR/in/platform/$PLATFORM/$BINARY"
 	done
-	if [ -e $TMPDIR/out ]
+	if [ -e "$TMPDIR/out" ]
 	then
 		# Move platform directory
-		mv $TMPDIR/in/platform/$PLATFORM $TMPDIR/out/platform/
+		mv "$TMPDIR/in/platform/$PLATFORM" "$TMPDIR/out/platform/"
 
 		# Merge install.rdf targetPlatform lines
-		dos2unix -q $TMPDIR/in/install.rdf
-		if diff $TMPDIR/out/install.rdf $TMPDIR/in/install.rdf | grep "^[<>]" | grep -v targetPlatform
+		dos2unix -q "$TMPDIR/in/install.rdf"
+		if diff "$TMPDIR/out/install.rdf" "$TMPDIR/in/install.rdf" | grep "^[<>]" | grep -v targetPlatform
 		then
 			echo "Warning: Differences in install.rdf files other than targetPlatform." 1>&2
 		fi
-		diff --ifdef=DELETEME $TMPDIR/out/install.rdf $TMPDIR/in/install.rdf | grep -v "^#" > $TMPDIR/install.rdf
-		mv $TMPDIR/install.rdf $TMPDIR/out/install.rdf
+		diff --ifdef=DELETEME "$TMPDIR/out/install.rdf" "$TMPDIR/in/install.rdf" | grep -v "^#" > "$TMPDIR/install.rdf"
+		mv "$TMPDIR/install.rdf" "$TMPDIR/out/install.rdf"
 
 		# XXX Warn if there are other differences?
 
 		# Remove the rest
-		rm -rf $TMPDIR/in
+		rm -rf "$TMPDIR/in"
 	else
-		mv $TMPDIR/in $TMPDIR/out
+		mv "$TMPDIR/in" "$TMPDIR/out"
 	fi
 
 	# Fix the targetPlatform we generated for Universal binaries to
 	# represent only what is really supported.
-	cat $TMPDIR/out/install.rdf | sed '/<em:targetPlatform>Darwin</{ h; s/<em:targetPlatform>Darwin</<em:targetPlatform>Darwin_x86-gcc3</; G; s/<em:targetPlatform>Darwin</<em:targetPlatform>Darwin_ppc-gcc3</}' > TMPDIR/install.rdf
-	mv $TMPDIR/install.rdf $TMPDIR/out/install.rdf
+	cat "$TMPDIR/out/install.rdf" | sed '/<em:targetPlatform>Darwin</{ h; s/<em:targetPlatform>Darwin</<em:targetPlatform>Darwin_x86-gcc3</; G; s/<em:targetPlatform>Darwin</<em:targetPlatform>Darwin_ppc-gcc3</}' > "$TMPDIR/install.rdf"
+	mv "$TMPDIR/install.rdf" "$TMPDIR/out/install.rdf"
 done
 
-(cd $TMPDIR/out && zip ../out.zip $(find . -type f))
-mv $TMPDIR/out.zip $DEST
+(cd "$TMPDIR/out" && zip ../out.zip $(find . -type f))
+mv "$TMPDIR/out.zip" "$DEST"
 
 umask $OLDUMASK
-rm -rf $TMPDIR
+rm -rf "$TMPDIR"
