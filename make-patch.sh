421ba70244d4e678f7ab4991f52801769f7b303a
L. David Baron <dbaron@dbaron.org>
2009-04-17 17:19:47 -0700

    Update make-patch.sh to generate diffs from hg source, output diff in more hg-like format (requires -p1 instead of -p0 now), and also fix extra output from popd.

--- make-patch.sh
+++ make-patch.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
+# Which source should we patch?
+MOZRELEASE=http://hg.mozilla.org/releases/mozilla-1.9.1/
+MOZREV=FIREFOX_3_1b3_RELEASE  # any revision syntax that hg accepts
+
 TMPDIR="$(mktemp -d)" || exit 1
 pushd "$TMPDIR" > /dev/null 2>&1 || exit 1
-cvs -d':pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot' co mozilla/browser/confvars.sh mozilla/browser/installer/windows/packages-static mozilla/browser/installer/unix/packages-static > /dev/null 2>&1 || exit 1
-cd mozilla || exit 1
-perl -pi -e 's/MOZ_EXTENSIONS_DEFAULT=" /MOZ_EXTENSIONS_DEFAULT=" leak-monitor /' browser/confvars.sh || exit 1
-cat >> browser/installer/windows/packages-static <<EOM
+
+for FILE in "browser/confvars.sh" "browser/installer/windows/packages-static" "browser/installer/unix/packages-static"
+do
+    mkdir -p "$(echo "$FILE" | sed 's,/[^/]*$,,;s,^,a/,')"
+    wget -q "$MOZRELEASE/raw-file/$MOZREV/$FILE" -O "a/$FILE"
+done
+
+cp -r a b
+
+perl -pi -e 's/MOZ_EXTENSIONS_DEFAULT=" /MOZ_EXTENSIONS_DEFAULT=" leak-monitor /' b/browser/confvars.sh || exit 1
+cat >> b/browser/installer/windows/packages-static <<EOM
 bin\\extensions\\{1ed6b678-1f93-4660-a9c5-01af87b323d3}\\components\\leakmon.dll
 bin\\extensions\\{1ed6b678-1f93-4660-a9c5-01af87b323d3}\\components\\leakmonitor.xpt
 bin\\extensions\\{1ed6b678-1f93-4660-a9c5-01af87b323d3}\\chrome\\leakmon.jar
 bin\\extensions\\{1ed6b678-1f93-4660-a9c5-01af87b323d3}\\chrome.manifest
 bin\\extensions\\{1ed6b678-1f93-4660-a9c5-01af87b323d3}\\install.rdf
 EOM
-cat >> browser/installer/unix/packages-static <<EOM
+cat >> b/browser/installer/unix/packages-static <<EOM
 bin/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome.manifest
 bin/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/components/leakmonitor.xpt
 bin/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/components/libleakmon.so
 bin/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/install.rdf
 bin/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon.jar
 EOM
-cvs -d':pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot' diff
-popd
+diff -u -r a b
+popd > /dev/null
 rm -rf "$TMPDIR"
 
 DATE="$(LC_TIME=C TZ=UTC date +'%-d %b %Y %H:%M:%S') -0000"
@@ -28,12 +39,9 @@ DATE="$(LC_TIME=C TZ=UTC date +'%-d %b %Y %H:%M:%S') -0000"
 find . -type f | grep -v ".svn" | sed 's/^\.\///' | while read FNAME
 do
         cat <<EOM
-Index: extensions/leak-monitor/$FNAME
-===================================================================
-RCS file: extensions/leak-monitor/$FNAME
-diff -N extensions/leak-monitor/$FNAME
+diff -N b/extensions/leak-monitor/$FNAME
 --- /dev/null	1 Jan 1970 00:00:00 -0000
-+++ extensions/leak-monitor/$FNAME	$DATE
++++ b/extensions/leak-monitor/$FNAME	$DATE
 EOM
         LEN=$(wc -l "$FNAME" | cut -d' ' -f1)
         if [ "$LEN" == "0" ]
