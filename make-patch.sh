75b177a13715f11eb996d5ffd98097f01b322dbf
L. David Baron <dbaron@dbaron.org>
2011-02-17 17:58:29 -0800

    And just explicitly list all the files, which seems to work.

--- make-patch.sh
+++ make-patch.sh
@@ -18,7 +18,29 @@ cp -r a b
 perl -pi -e 's/MOZ_EXTENSIONS_DEFAULT=" /MOZ_EXTENSIONS_DEFAULT=" leak-monitor /' b/browser/confvars.sh || exit 1
 cat >> b/browser/installer/package-manifest.in <<EOM
 
-@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/*
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/components/interfaces.manifest
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/components/components.manifest
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/components/@DLL_PREFIX@leakmon@DLL_SUFFIX@
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/components/leakmonitor.xpt
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome.manifest
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/zh-TW/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/en-US/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/cs/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/it/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/ru/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/es-ES/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/de/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/ca/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/fr/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/hu/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/uk/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/ja/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/nl/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/locale/el/leakAlert.dtd
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/content/tree-utils.js
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/content/leakAlert.js
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/chrome/leakmon/content/leakAlert.xul
+@BINPATH@/extensions/{1ed6b678-1f93-4660-a9c5-01af87b323d3}/install.rdf
 EOM
 diff -u -r a b
 popd > /dev/null
